11624cd7dd996dc79f8488912f351526d7da7b44
Ricky Garcia <rickyg08@msn.com>
2020-10-19 10:43:38 -0600

    Added livedata

--- app/src/main/java/edu/cnm/deepdive/animals/viewmodel/AnimalViewModel.java
+++ app/src/main/java/edu/cnm/deepdive/animals/viewmodel/AnimalViewModel.java
@@ -30,7 +30,7 @@ public class AnimalViewModel extends AndroidViewModel {
     return animals;
   }
 
-  public MutableLiveData<Throwable> getThrowable() {
+  public LiveData<Throwable> getThrowable() {
     return throwable;
   }
 
